626fdf5c0c90135fd8b983b9f4e5de23cfe60a2c
Trixek24 <121521201+Trixek24@users.noreply.github.com>
2024-02-03 10:30:38 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,21 +1,39 @@
+//Game Functionality//
+
 var score = 0;
 var ppc = 1;
+var sound = new Audio('Keypress.wav');
+var soundisplaying = true;
 
-window.addEventListener("keydown", my_keydown);
+window.addEventListener("keyup", my_keyup);
 
-function my_keydown(e)
+function my_keyup(e)
 {
     keyPressed = e.keyCode;
     console.log(keyPressed);
 
-if(keyPressed == "32")
+if(keyPressed == "32") 
 {
+    if(soundisplaying == true){
+    sound.play();
+    console.log("sound is playing");
+    }
     console.log("SpaceBar was pressed");
-    document.getElementById("score").innerHTML = score;
+    document.getElementById("score").innerHTML = "$"+score;
     score = score + ppc;
     console.log("Score = " + score);
     localStorage.setItem("score",score)
+    
 }
+}
+
+function audio()
+{
+    if(soundisplaying == true){
+        soundisplaying = false;
+        console.log("sound is not playing");
+        document.getElementById("sound_btn").style.backgroundImage = "url('audio-off.png')";
+    }
 }
 
 function ppcDefine(){
@@ -24,12 +42,47 @@ function ppcDefine(){
 
 function plus1Point()
 {
-    if(score >= 500)
+    if(score >= 250)
     {
         ppc = ppc + 1;
         document.getElementById("ppc").innerHTML = ppc;
-        console.log("Points Per Second is "+ ppc)
+        console.log("Points Per Second is "+ ppc);
+        score = score - 250;
+        return;
+    }
+    else
+    {
+        console.log("Score Requirement not reached")
+        return;
+    }
+
+}
+
+function plus2Point()
+{
+    if(score >= 500)
+    {
+        ppc = ppc + 2;
+        document.getElementById("ppc").innerHTML = ppc;
+        console.log("Points Per Second is "+ ppc);
         score = score - 500;
+        return;
+    }
+    else
+    {
+        console.log("Score Requirement not reached")
+        return;
+    }
+
+}
+function plus5Point()
+{
+    if(score >= 1000)
+    {
+        ppc = ppc + 5;
+        document.getElementById("ppc").innerHTML = ppc;
+        console.log("Points Per Second is "+ ppc);
+        score = score - 1000;
         return;
     }
     else
@@ -51,4 +104,28 @@ function yellow()
 function blue()
 {
     document.body.style.backgroundColor = "blue";
+}
+function orange()
+{
+    document.body.style.backgroundColor = "orange";
+}
+function green()
+{
+    document.body.style.backgroundColor = "green";
+}
+function pink()
+{
+    document.body.style.backgroundColor = "pink";
+}
+function purple()
+{
+    document.body.style.backgroundColor = "purple";
+}
+function black()
+{
+    document.body.style.backgroundColor = "black";
+}
+function Default()
+{
+    document.body.style.backgroundColor = "grey";
 }
